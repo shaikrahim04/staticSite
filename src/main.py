e82096c42985df98027feeb690829e0bacdcb07e
shaikrahim04 <theshaikrahim@gmail.com>
2025-07-12 15:19:56 +0000

    fix: Minor fix

--- src/main.py
+++ src/main.py
@@ -47,7 +47,7 @@ def generate_page(from_path, template_path, dest_path, basepath):
     title = extract_title(markdown_content) or ""
 
     page_content = template_content.replace("{{ Title }}", title).replace("{{ Content }}", html_content)
-    page_content.replace('href="/', f'href="{basepath}').replace('src="/', f'src={basepath}')
+    page_content = page_content.replace('href="/', f'href="{basepath}').replace('src="/', f'src={basepath}')
 
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
 
